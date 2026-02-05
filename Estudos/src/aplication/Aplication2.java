7d954822b89565b02e6ab4d4ccd0307101212657
SrLobato <pedrolobato70@gmail.com>
2026-02-04 21:34:48 -0300

    test

---
+++ Estudos/src/aplication/Aplication2.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import entities.Employee;
+
+public class Aplication2 {
+
+	public static void main(String[] args) {
+		
+		Employee teste = new Employee(55, "Pedro", 10.0);
+		
+		teste.gritar();
+
+	}
+
+}
